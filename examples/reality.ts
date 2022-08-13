75a4b25d34247d278e39aede3435ac946ff7c18f
istrav <travis.burandt@gmail.com>
2022-08-13 16:38:48 -0500

    an

--- examples/reality.ts
+++ examples/reality.ts
@@ -1,35 +1,38 @@
-// continuum
+// past present future; now.
 // space: [1, 2, 3]
-// time: 0
+// time: 4
 
 import { Universe, Multiverse } from '../index'
+
+// continuum
+const life: number = 1
+const death: number = 4
 
 // singleton
 let universe = Universe.getInstance() 
 
-// meaning
-const life: number = 1
-const death: number = 4
-
 // prototypes
 let multiverses: Array<Multiverse> = [] 
 
-// generate multiverses using spawn and respawn
+// clone prototypes using spawn and respawn
 let firstMultiverse: Multiverse = Multiverse.spawn(universe, life, new Date())
 multiverses.push(firstMultiverse)
 
+// modulus
+let daysPerYear = 365
 function addDays(date, days) {
   var result = new Date(date);
   result.setDate(result.getDate() + days);
   return result;
 }
 
+// live from prototype to prototype
 let currentMultiverse: Multiverse = firstMultiverse
 for (let age = life + 1; age <= death; age++) {
   currentMultiverse = Multiverse.respawn(
     currentMultiverse, 
     age, 
-    addDays(new Date(), 365 * age - 365)
+    addDays(new Date(), daysPerYear * age - daysPerYear)
   )
   multiverses.push(currentMultiverse)
 }
